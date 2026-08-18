a8778e89f8ec85f54890372c2836a9a18d5c1e04
arin30 <43118055+arin30@users.noreply.github.com>
2026-08-18 10:31:56 -0700

    Add POST support to test backend

--- examples/backend.py
+++ examples/backend.py
@@ -1,4 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         body = f"backend received {self.path}\n".encode()
@@ -7,6 +9,25 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header("Content-Length", str(len(body)))
         self.end_headers()
         self.wfile.write(body)
+
+    def do_POST(self):
+        length = int(self.headers.get("Content-Length", "0"))
+        remaining = length
+        while remaining:
+            chunk = self.rfile.read(min(16384, remaining))
+            if not chunk:
+                break
+            remaining -= len(chunk)
+        received = length - remaining
+        body = f"backend received {received} bytes\n".encode()
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", str(len(body)))
+        self.end_headers()
+        self.wfile.write(body)
+
     def log_message(self, *_):
         pass
+
+
 HTTPServer(("127.0.0.1", 9000), Handler).serve_forever()
